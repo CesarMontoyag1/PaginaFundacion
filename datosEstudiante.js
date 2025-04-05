494e1780ee6985a9afdac01dd8889f16d867b7e5
CesarMontoyag1 <159492748+CesarMontoyag1@users.noreply.github.com>
2025-04-05 16:42:07 -0500

    Add files via upload

---
+++ datosEstudiante.js
@@ -0,0 +1,22 @@
+class Estudiante {
+    constructor(nombre, grupoEtnico, zonaEstudiante, apellidos, factorVulnerabilidad, mundo, genero, paisNacimiento, modalidad, fechaNacimiento, municipioResidencia, dias, estadoCivil, direccionResidencia, horarioInicio, codigoDane, municipioNacimiento, horarioFin) {
+        this.nombre = nombre;
+        this.grupoEtnico = grupoEtnico;
+        this.zonaEstudiante = zonaEstudiante;
+        this.apellidos = apellidos;
+        this.factorVulnerabilidad = factorVulnerabilidad;
+        this.mundo = mundo;
+        this.genero = genero;
+        this.paisNacimiento = paisNacimiento;
+        this.modalidad = modalidad;
+        this.fechaNacimiento = fechaNacimiento;
+        this.municipioResidencia = municipioResidencia;
+        this.dias = dias;
+        this.estadoCivil = estadoCivil;
+        this.direccionResidencia = direccionResidencia;
+        this.horarioInicio = horarioInicio;
+        this.codigoDane = codigoDane;
+        this.municipioNacimiento = municipioNacimiento;
+        this.horarioFin = horarioFin;
+    }
+}
